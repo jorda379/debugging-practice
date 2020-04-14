eb2cf0a3b18f42fd4706c17060e77e42e578af82
jorda379 <60120307+jorda379@users.noreply.github.com>
2020-04-14 22:05:51 +0200

    Modified the value card3 to the value card2

--- exercise/scripts/main.js
+++ exercise/scripts/main.js
@@ -33,8 +33,9 @@ window.addEventListener('DOMContentLoaded', function() {
         // Regardless of whether or not the cards match, deselect them and reset
         // the collection of matched cards.
         card1.classList.remove('is-selected');
-        card3.classList.remove('is-selected');
+        card2.classList.remove('is-selected');
         selectedCards = [];
+
       }
 
       // If we've matched all the cards, display a message.
